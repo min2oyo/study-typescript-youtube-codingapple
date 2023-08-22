e72c8f42a078f6e9508eaa49c01ea63eb958d7ce
min2oyo <min2u@outlook.com>
2023-08-22 14:27:19 +0900

    타입스크립트 기본 타입 정리 (primitive types)

---
+++ dist/01. primitive types.js
@@ -0,0 +1,8 @@
+// Type은 알아서 지정해주므로, 고수는 type 지정을 안 함
+let 이름 = `kim`;
+let 나이 = 50;
+let 결혼했니 = true;
+let 결혼했니2 = null;
+let 결혼했니3 = undefined;
+let 회원들 = [`kim`, `park`];
+let 회원들2 = { membr1: `kim`, member2: `park` };
