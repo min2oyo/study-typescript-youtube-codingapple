417006fafdd04abf7af99e4cec452ab24f53bf86
min2oyo <min2u@outlook.com>
2023-08-23 18:51:37 +0900

    public, private 쓰는거 보니까 타입스크립트 귀여운편

--- index.js
+++ index.js
@@ -573,3 +573,24 @@
 // let 함수4 = function () { // type: never
 //   throw new Error();
 // };
+// /** 오리지널 자바스크립트 문법에서도 #이걸 속성옆에 붙이면 private 속성이 됩 */
+// // 개념
+// class User {
+//   public name;  // 모든 자식들이 이용 가능  // 생략 가능
+//   private familyName = `kim`;
+//   constructor(name: string) {
+//     this.name = name + this.familyName;
+//   }
+//   changeSecret() {
+//     this.familyName = `park`;
+//   }
+// }
+// let 유저1 = new User(`park`);
+// // 유저1.familyName;  // 사용 못 함
+// 유저1.changeSecret(); // 사용 가능
+// // 축약
+// class Person {
+//   constructor(public name: string) { };
+// }
+// let 자식 = new Person(`kim`);
+// console.log(`자식`, 자식);
